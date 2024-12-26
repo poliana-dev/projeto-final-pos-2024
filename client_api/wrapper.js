8dda5304c39e15a7293736e71a6f455a67d183f3
poliana-dev <m.polisys02@gmail.com>
2024-12-26 15:12:36 -0300

    feat: atualização 🤓

--- client_api/wrapper.js
+++ client_api/wrapper.js
@@ -27,4 +27,33 @@ export const fetchFilmePersonagens = async (id) => {
   return data;
 };
 
+export const fetchFilmeAcessorio = async (id) => {
+  const response = await fetch(`http://127.0.0.1:8000/acessorios/${id}`);
+  const data = await response.json();
+  return data;
+};
+
+
+export async function fetchCenario() {
+  const url = `${api_url}cenarios`; 
+  const response = await fetch(url);  
+  const data = await response.json();
+  return data;
+};
+
+export async function fetchMusica() {
+  const url = `${api_url}musicas`; 
+  const response = await fetch(url);  
+  const data = await response.json();
+  return data;
+};
+
+export async function fetchFilmesMusica(id) {
+  const url = `${api_url}filmes/${id}`; 
+  const response = await fetch(url);  
+  const data = await response.json();
+  return data;
+}
+
+
 
